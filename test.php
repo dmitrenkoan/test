e7349b60071ecdac6e54ce76332684d7a6805da4
dmitrenkoan <dmitrenkoan90@gmail.com>
2018-03-31 22:09:11 +0300

    test commit

--- test.php
+++ test.php
@@ -1,1 +1,48 @@
-<?echo 'hi'?>
+<?
+/*function doSomething( &$arg )
+{
+$return = $arg;
+$arg += 1;
+return $return;
+}
+
+$a = 3;
+$b = doSomething( $a );
+echo 'a:'.$a;
+echo 'b:'.$b;
+//...what is the value of $a and $b after the function call and why?
+*/
+
+/*
+//4. What is wrong with this code:
+class SomeClass
+{
+protected $_someMember;
+
+public function __construct()
+{
+$this->_someMember = 1;
+}
+
+public static function getSomethingStatic()
+{
+return $this->_someMember * 5;
+}
+}*/
+/*
+//5. Given this code:
+class A {
+public $b = 2;
+}
+
+function test1($a) {
+$a->b = 7;
+}
+$a = new A();
+test1($a);
+echo $a->b;*/
+
+
+
+
+?>
